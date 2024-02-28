c70e4b912ac023e08497f9917c8ada72777aa094
20035 <20035@wc.school.nz>
2024-02-29 09:52:32 +1300

    Database connected

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
 
+DATABASE= "C:/Users/20035/PycharmProjects/Smile2/cafe.db"
+
+def create_connection(db_file):
+    try:
+        connection = sqlite3.connect(db_file)
+        return connection
+    except Error as e:
+        print(e)
+    return None
 
 @app.route('/')
 def render_homepage():
@@ -10,6 +21,13 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
+    con = create_connection(DATABASE)
+    query = "SELECT name, description, volume, image, price FROM products"
+    cur= con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    print(product_list)
+    con.close()
     return render_template('menu.html')
 
 
